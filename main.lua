0703f808e86500b8fb54ca9f9215a92dba311911
Ben Fridman <benf3632@gmail.com>
2022-06-10 11:53:54 +0300

    Add Ball and Paddle

--- main.lua
+++ main.lua
@@ -1,14 +1,55 @@
 push = require 'push'
 
+Class = require 'class'
+
+require 'Paddle'
+
+require 'Ball'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    -- disable antialiasing
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- init fonts
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    largeFont = love.graphics.newFont('font.ttf', 16)
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+    love.graphics.setFont(smallFont)
+
+    -- setup virtual screen
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
+        resizable = true,
         vsync = true,
-        resizable  = false,
     })
+
+    -- init game vars
+    player1Score = 0
+    player2Score = 0
+
+    servingPlayer = 1
+
+    player1 = Paddle(10, 30, 5, 20)
+    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
+    ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+    gameState = 'start'
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
 end
 
 function love.update()
@@ -18,13 +59,20 @@ end
 function love.draw()
     push:start()
 
-    love.graphics.printf(
-        'Hello Pong!',
-        0,
-        WINDOW_HEIGHT / 2 - 6,
-        WINDOW_WIDTH,
-        'center'
-    )
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    if gameState == 'start' then
+        love.graphics.setFont(smallFont)
+        love.graphics.printf("Welcome to Pong!", 0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf("Press Enter to begin!", 0, 20, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'serve' then
+    elseif gameState == 'play' then
+    elseif gameState == 'done' then
+    end
+
+    player1:render()
+    player2:render()
+    ball:render()
 
     push:finish()
 end
