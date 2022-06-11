121837e8fe52b24484ddc47363b886b0c67a0fbf
Ben Fridman <benf3632@gmail.com>
2022-06-12 00:18:03 +0300

    Add paddle movment and display score

--- main.lua
+++ main.lua
@@ -12,6 +12,8 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 function love.load()
     -- disable antialiasing
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -45,6 +47,25 @@ end
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'serve'
+        elseif gameState == 'server' then
+            gameState = 'play'
+        elseif gameState == 'done' then
+            gameState = 'serve'
+
+            ball:reset()
+
+            player1Score = 0
+            player2Score = 0
+
+            if winningPlayer == 1 then
+                servingPlayer = 2
+            else
+                servingPlayer = 1
+            end
+        end
     end
 end
 
@@ -52,14 +73,38 @@ function love.resize(w, h)
     push:resize(w, h)
 end
 
-function love.update()
+function love.update(dt)
+    if gameState == 'play' then
 
+    end
+
+    if love.keyboard.isDown('w') then
+        player1.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('s') then
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
+    end
+
+
+    if love.keyboard.isDown('up') then
+        player2.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('down') then
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
+    end
+
+    player1:update(dt)
+    player2:update(dt)
 end
 
 function love.draw()
     push:start()
 
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    displayScore()
 
     if gameState == 'start' then
         love.graphics.setFont(smallFont)
@@ -75,4 +120,11 @@ function love.draw()
     ball:render()
 
     push:finish()
+end
+
+
+function displayScore()
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 end
